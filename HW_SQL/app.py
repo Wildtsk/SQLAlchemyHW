143b1d0ec71ecb5ae16f104e496dd0eadc05aaf0
Wildtsk <wildtsk@gmail.com>
2022-08-16 12:45:14 +0300

    "Исправила недочеты, выполнены 2 этапа"

--- HW_SQL/app.py
+++ HW_SQL/app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import jsonify, Flask
 
 from utils import get_by_title_release_year, get_by_title
@@ -9,18 +11,32 @@ app.config["JSON_AS_ASCII"] = False
 @app.get('/movie/<title_name>/')
 def search_by_title_page(title_name):
     try:
-        return jsonify(get_by_title(title_name))
+        result = get_by_title(title_name)
+        return app.response_class(
+            response=json.dumps(result,
+                                ensure_ascii=False,
+                                indent=4),
+            status=200,
+            mimetype="application/json"
+        )
     except TypeError:
         return f'Фильм с назвнаием {title_name} я не нашел('
 
 
-@app.get('/movie/<int:release_year_one>/to/<int:release_year_two>.')
+@app.get('/movie/<int:release_year_one>/to/<int:release_year_two>')
 def search_by_release_year_page(release_year_one, release_year_two):
     try:
-        return jsonify(get_by_title_release_year(release_year_one, release_year_two))
+        result = get_by_title_release_year(release_year_one, release_year_two)
+        return app.response_class(
+            response=json.dumps(result,
+                                ensure_ascii=False,
+                                indent=4),
+            status=200,
+            mimetype="application/json"
+        )
     except TypeError:
         return f'Фильм с годами {release_year_one, release_year_two} я не нашел('
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='localhost', port=5000, debug=True)
--- HW_SQL/utils.py
+++ HW_SQL/utils.py
@@ -1,39 +1,38 @@
 import sqlite3
 
 
-def get_by_title(title_name):
+def get_value_from_db(sql):
     with sqlite3.connect("netflix.db") as connection:
-        cursor = connection.cursor()
-        query = f"""
-                SELECT title, country, release_year, listed_in, description
-                FROM netflix
-                WHERE title LIKE '%{title_name}%'
-                ORDER BY release_year DESC
-        """
-        cursor.execute(query)
-        result = cursor.fetchone()
-        film = {
-            'title' : result[0],
-            'country' : result[1],
-            'release_year': result[2],
-            'listed_in': result[3],
-            'description': result[4],
-        }
-    return film
+        connection.row_factory = sqlite3.Row
+
+        result = connection.execute(sql).fetchall()
+
+        return result
+
+
+def get_by_title(title_name):
+
+    sql = f"""
+          SELECT title, country, release_year, listed_in, description
+          FROM netflix
+          WHERE title LIKE '{title_name}'
+          ORDER BY release_year DESC
+    """
+    result = get_value_from_db(sql)
+    for item in result:
+        return dict(item)
 
 
 def get_by_title_release_year(release_year_one, release_year_two):
-    with sqlite3.connect("netflix.db") as connection:
-        cursor = connection.cursor()
-        query = f"""
-                SELECT title, release_year
-                FROM netflix
-                WHERE release_year BETWEEN '{release_year_one}' AND '{release_year_two}'
-        """
-        cursor.execute(query)
-        result = cursor.fetchall()
-        film = {
-            'title' : result[0],
-            'release_year' : result[1],
-        }
-    return film
+    sql = f"""
+            SELECT title, release_year
+            FROM netflix
+            WHERE release_year BETWEEN {release_year_one} AND {release_year_two}
+    """
+    result = get_value_from_db(sql)
+    dict_result = []
+    for item in result:
+        dict_result.append(dict(item))
+    return dict_result
+
+# print(get_by_title_release_year(1999, 2001))
