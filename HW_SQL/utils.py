3daf3bc7383652ac58f6ed2bb471103cf92c448c
Wildtsk <wildtsk@gmail.com>
2022-08-16 14:44:18 +0300

    5 этап слегка запутал, 6 самый легкий)

--- HW_SQL/utils.py
+++ HW_SQL/utils.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-def get_value_from_db(sql):
+def get_value_from_db(sql): #Читает таблицу и выносит определенные результаты
     with sqlite3.connect("netflix.db") as connection:
         connection.row_factory = sqlite3.Row
 
@@ -10,7 +10,7 @@ def get_value_from_db(sql):
         return result
 
 
-def get_by_title(title_name):
+def get_by_title(title_name):#Поиск фильмов по названию, выводит один результат
     sql = f"""
           SELECT title, country, release_year, listed_in, description
           FROM netflix
@@ -22,7 +22,7 @@ def get_by_title(title_name):
         return dict(item)
 
 
-def get_by_title_release_year(release_year_one, release_year_two):
+def get_by_title_release_year(release_year_one, release_year_two):#Поиск фильмов по релизу. Временной диапазон
     sql = f"""
             SELECT title, release_year
             FROM netflix
@@ -35,7 +35,7 @@ def get_by_title_release_year(release_year_one, release_year_two):
     return dict_result
 
 
-def get_by_rating(rating):
+def get_by_rating(rating): #Поиск фильмов по рейтингу, по умолчанию "G"
     my_rating = {
         "children": ("G"),
         "family": ("G", "PG", "PG-13"),
@@ -54,12 +54,12 @@ def get_by_rating(rating):
     return dict_result
 
 
-def get_by_genre(genre):
+def get_by_genre(genre): #Поиск фильмов по жанрам
 
     sql = f"""
             SELECT title, description
             FROM netflix
-            WHERE listed_in LIKE '{genre}'
+            WHERE listed_in LIKE '%{str(genre).title()}%'
     """
     result = get_value_from_db(sql)
     dict_result = []
@@ -68,4 +68,42 @@ def get_by_genre(genre):
     return dict_result
 
 
-print(get_by_genre("Movies"))
+def get_by_name(name1="Jack Black", name2="Dustin Hoffman"):#Поиск актеров которые снимались с 2 определенными, больше 2 раз
+
+    sql = f"""
+            SELECT "cast"
+            FROM netflix
+            WHERE "cast" LIKE '%{name1}%' AND "cast" LIKE '%{name2}%'
+    """
+    result = get_value_from_db(sql)
+    names_list = []
+    dict_result = {}
+    for item in result:
+        names = set(dict(item).get("cast").split(", ")) - set([name1, name2])
+
+        for name in names:
+            dict_result[name.strip()] = dict_result.get(name.strip(), 0) + 1
+
+    for key, value in dict_result.items():
+        if value > 2:
+            names_list.append(key)
+
+    return names_list
+
+
+def get_by_type(type_movie, release, genre):#Поиск фильмов по 3 параментрам: жанр, тип, релиз
+
+    sql = f"""
+            SELECT title, description
+            FROM netflix
+            WHERE type like '{type_movie}'
+            AND release_year = '{release}'
+            AND listed_in LIKE '%{str(genre).title()}%'
+    """
+    result = get_value_from_db(sql)
+    dict_result = []
+    for item in result:
+        dict_result.append(dict(item))
+    return dict_result
+
+
