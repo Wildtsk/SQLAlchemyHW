9ca166984a7391202e1292aa2036e1775f552474
Wildtsk <wildtsk@gmail.com>
2022-08-30 08:14:16 +0300

    Метод POST не работает(

--- SQLAlchemyHW/main.py
+++ SQLAlchemyHW/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey, Column, Integer, String, Text
 
 from utils import get_users_all, get_orders_all, get_offers_all
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
@@ -22,6 +23,17 @@ class User(db.Model):
     email = Column(Text)
     role = Column(Text)
     phone = Column(Text)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+            "email": self.email,
+            "role": self.role,
+            "phone": self.phone,
+        }
 
     def __repr__(self):
         return f"User({self.id}, {self.first_name}, {self.last_name}, {self.age},{self.email},{self.role},{self.phone})"
@@ -45,6 +57,17 @@ class Order(db.Model):
     customer = db.relationship("User", foreign_keys=[customer_id])
     executor = db.relationship("User", foreign_keys=[executor_id])
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "address": self.address,
+            "price": self.price,
+        }
+
     def __repr__(self):
         return f"Order({self.id}, {self.name}, {self.description}, {self.start_date}, {self.end_date}, {self.address}, {self.price})"
 
@@ -59,6 +82,13 @@ class Offer(db.Model):
 
     order_1 = db.relationship("Order")
     user = db.relationship("User")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "executor_id": self.executor_id,
+        }
 
     def __repr__(self):
         return f"Offer({self.id})"
@@ -78,7 +108,6 @@ with db.session.begin():
     db.session.add_all(order)
     db.session.add_all(offer)
 
-
 db.session.commit()
 
 # Для проверки получаем данные из БД
@@ -86,18 +115,24 @@ order = db.session.query(Order).get(1)
 offer = db.session.query(Offer).get(1)
 user = db.session.query(User).get(1)
 
-# print(order)
-# print(order.customer)
-# print(order.executor)
 
-# print(db.session.query(Offer).get(1).db.session.query(Order).get(1))
-# print(offer.user)
-# print(user)
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == "GET":
+        result = []
+        for us in db.session.query(User).all():
+            result.append(us.to_dict())
+        return jsonify(result), 200
+    elif request.method == "POST":
+        content = request.json
+        user_add_all = [User(**row) for row in content]
+        db.session.add(user_add_all)
+        db.session.commit()
 
-
-@app.route('/users', methods=['GET'])
-def read_users():
-    return f'{db.session.query(User).all()}'
+        result = []
+        for us in db.session.query(User).all():
+            result.append(us.to_dict())
+        return jsonify(result), 200
 
 
 @app.route('/users/<int:pk>', methods=['GET'])
@@ -105,9 +140,23 @@ def read_user(pk):
     return f'{db.session.query(User).get(pk)}'
 
 
-@app.route('/orders', methods=['GET'])
-def read_orders():
-    return f'{db.session.query(Order).all()}'
+@app.route('/orders', methods=["GET", "POST"])
+def orders():
+    if request.method == "GET":
+        result = []
+        for order_1 in db.session.query(Order).all():
+            result.append(order_1.to_dict())
+        return jsonify(result), 200
+    elif request.method == "POST":
+        content = request.json
+        order_add_all = [Order(**row) for row in content]
+        db.session.add(order_add_all)
+        db.session.commit()
+
+        result = []
+        for order_1 in db.session.query(Order).all():
+            result.append(order_1.to_dict())
+        return jsonify(result), 200
 
 
 @app.route('/orders/<int:pk>', methods=['GET'])
@@ -116,31 +165,27 @@ def read_order(pk):
 
 
 @app.route('/offers', methods=['GET'])
-def read_offers():
-    return f'{db.session.query(Order).all()}\n{db.session.query(User).all()}'
+def offers():
+    if request.method == "GET":
+        result = []
+        for offer_1 in db.session.query(Offer).all():
+            result.append(offer_1.to_dict())
+        return jsonify(result), 200
+    elif request.method == "POST":
+        content = request.json
+        offer_add_all = [Offer(**row) for row in content]
+        db.session.add(offer_add_all)
+        db.session.commit()
+
+        result = []
+        for offer_1 in db.session.query(Offer).all():
+            result.append(offer_1.to_dict())
+        return jsonify(result), 200
 
 
 @app.route('/offers/<int:pk>', methods=['GET'])
 def read_offer(pk):
     return f'{db.session.query(Offer).get(pk).order_1}\n{db.session.query(Offer).get(pk).user}'
-
-
-@app.route('/users', methods=['POST'])
-def post_user():
-    content = request.form.get('content')
-    user_add = [User(**row) for row in content]
-    db.session.add(user_add)
-    db.session.commit()
-    return "Данные успешно сохранены"
-
-
-# @app.route('/users/<int:pk>', methods=['PUT'])
-# def put_user(pk):
-#     content = request.form.get('content')
-#     user_add = [User(**row) for row in content]
-#     db.session.add(user_add)
-#     db.session.commit()
-#     return "Данные успешно сохранены"
 
 
 if __name__ == "__main__":
--- SQLAlchemyHW/utils.py
+++ SQLAlchemyHW/utils.py
@@ -1,4 +1,4 @@
-from flask import json, jsonify
+from flask import json, jsonify, Response
 
 
 def get_users_all() -> list[dict]:
